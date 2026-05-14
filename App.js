de36a03cb62fb3a554f13663e4d07670fdf4de33
Ilyas Assfar <assfar35@gmail.com>
2026-05-14 14:21:01 +0100

    feat : host fixed

--- App.js
+++ App.js
@@ -20,3 +20,12 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 }); 
+
+app.get('/', (req, res) => {
+    res.send('Hello wolrd!');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
