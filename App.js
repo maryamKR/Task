ece99824991fe8fd00a641ceeac8ca5af445a4c3
Ilyas Assfar <assfar35@gmail.com>
2026-05-15 13:25:26 +0100

    feat : Frontend connected

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const path = require("path");
 require("dotenv").config();
 
 const taskRoutes = require("./routes/taskRoutes");
@@ -12,8 +13,23 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+// serve frontend
+app.use(express.static(path.join(__dirname, "frontend")));
+
+const BASE_API_URL = process.env.BASE_API_URL || "";
+app.get("/config.js", (req, res) => {
+  res.type("application/javascript");
+  const safeUrl = BASE_API_URL.replace(/'/g, "\\'");
+  res.send(`window.__BASE_API_URL__ = '${safeUrl}';`);
+});
+
 // routes
-app.use("/tasks", taskRoutes);
+app.use("/api/tasks", taskRoutes);
+
+// fallback to frontend for client-side routes
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 
